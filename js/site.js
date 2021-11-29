d126afb76ba2c3608c4aad7a4496e6c53cc47c14
leon sam <leo89_my@hotmail.com>
2021-11-29 23:00:48 +0800

    automate/scrap site.js

---
+++ js/site.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
+  const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/Web_scraping");
+  await page.screenshot({ path: "scraping.png" });
+  // await browser.waitForTarget(() => false);
+
+  const result = await page.evaluate(() => {
+    let headingFromWeb = document.querySelectorAll(".mw-headline");
+    const headingList = [...headingFromWeb];
+    return headingList.map((h) => h.innerText);
+  });
+
+  console.log(result);
+
+  await browser.close();
+})();
